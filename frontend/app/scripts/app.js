627b4c8efd349574400b6c0622df4e1954a6172f
Viet Trung <viettrung9012@yahoo.com>
2014-10-09 17:49:51 +0800

    initialize again

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+$(document).foundation();
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendApp', []);
