3e278537d7438a65226814c6d04fa0f3cbdf4b3f
viettrung9012 <viettrung9012@yahoo.com>
2014-10-18 17:21:26 +0800

    login with google implemented

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -10,5 +10,12 @@
  */
 angular
   .module('frontendApp', [
-    'ngTouch', 'ngDragDrop', 'ui.tree'
-  ]);
+    'ngTouch', 'ngDragDrop', 'ui.tree', 'satellizer'
+  ]).config(function($authProvider) {
+
+    $authProvider.google({
+      clientId: '353055755298-lvrfqibegsmulqs1hvqk6mdegu9ldegs.apps.googleusercontent.com'
+    });
+
+   
+});
--- frontend/app/scripts/controllers/header.js
+++ frontend/app/scripts/controllers/header.js
@@ -8,10 +8,27 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('HeaderCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('HeaderCtrl', function ($scope, $auth) {
+	$scope.awesomeThings = [
+		'HTML5 Boilerplate',
+		'AngularJS',
+		'Karma'
+	];
+	$scope.isAuthenticated = function() {
+      return $auth.isAuthenticated();
+    };
+    $scope.authenticate = function(provider) {
+      $auth.authenticate(provider)
+        .then(function() {
+          console.log('logged in');
+        })
+        .catch(function(response) {
+		  console.log(response);
+          console.log('logged in error');
+        });
+    };
+	$auth.logout()
+      .then(function() {
+        console.log('logged out');
+      });
+});
