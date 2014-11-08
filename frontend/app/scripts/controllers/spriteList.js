bc6c8a2ef7d8e055cdb10addc458284fcb54bd20
viettrung9012 <viettrung9012@yahoo.com>
2014-11-08 21:52:42 +0800

    clean up console log

--- frontend/app/scripts/controllers/spriteList.js
+++ frontend/app/scripts/controllers/spriteList.js
@@ -10,11 +10,8 @@ angular.module('frontendApp')
   		SpriteService.addSpriteList(spr.name, spr.image, 0, 0);
   	}
   	$scope.onDragComplete=function(data,evt){
-        console.log("drop success, data:", data);
-        console.log(window.innerWidth + " " + window.innerHeight);
         var divTop = window.innerHeight*1/10+20;
         var divLeft = window.innerWidth*1/2;
         SpriteService.addSpriteList(data.name, data.image, evt.tx-divLeft, evt.ty-divTop);
-        console.log(evt);
     }
   });
