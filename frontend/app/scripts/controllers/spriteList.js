ec7861c8d87d14b2ad7ff6edf47f4e5273e33773
darrenbkl <a0087547@nus.edu.sg>
2014-11-10 13:03:49 +0800

    Update spriteList.js

--- frontend/app/scripts/controllers/spriteList.js
+++ frontend/app/scripts/controllers/spriteList.js
@@ -8,10 +8,18 @@ angular.module('frontendApp')
   	$scope.list = SpriteService.getSpriteList();
   	$scope.addSprite = function(spr) {
   		SpriteService.addSpriteList(spr.name, spr.image, 0, 0);
-  	}
+  	};
   	$scope.onDragComplete=function(data,evt){
         var divTop = window.innerHeight*1/10+20;
         var divLeft = window.innerWidth*1/2;
         SpriteService.addSpriteList(data.name, data.image, evt.tx-divLeft, evt.ty-divTop);
-    }
+    };
+    $scope.sounds = [
+		{index: '0', name: 'cat'},
+		{index: '1', name: 'cow'},
+		{index: '2', name: 'goat'},
+		{index: '3', name: 'elephant'},
+		{index: '4', name: 'pig'},
+		{index: '5', name: 'hello'},
+	]
   });
